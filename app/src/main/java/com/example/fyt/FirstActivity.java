a23fc58ef6f3359fcc83546c58a614a2eb51dc7b
JQseventeen <2389631736@qq.com>
2024-10-04 23:06:15 +0800

    编辑登录界面，差注册

--- app/src/main/java/com/example/fyt/FirstActivity.java
+++ app/src/main/java/com/example/fyt/FirstActivity.java
@@ -1,12 +1,16 @@
 package com.example.fyt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fyt.ui.login.LoginActivity;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -15,6 +19,21 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first);
+
+        // 设置窗口内边距（如果需要）
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 使用 Handler 延迟 1000 毫秒（1 秒）后启动 LoginActivity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(FirstActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // 结束当前 Activity
+        }, 2000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/fyt/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.example.fyt.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.fyt.data.LoginDataSource;
+import com.example.fyt.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}

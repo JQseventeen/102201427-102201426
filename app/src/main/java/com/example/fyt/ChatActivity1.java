083bc9268240c56b5ac604c95e681b04abb732cf
JQseventeen <2389631736@qq.com>
2024-10-05 17:16:02 +0800

    底部导航栏2.0

--- app/src/main/java/com/example/fyt/ChatActivity1.java
+++ app/src/main/java/com/example/fyt/ChatActivity1.java
@@ -1,6 +1,8 @@
 package com.example.fyt;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class ChatActivity1 extends AppCompatActivity {
+
+    private ListView listViewProjects;
+    private Button buttonHome;
+    private Button buttonChat;
+    private Button buttonMy;
+    private Button buttonWarehouse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        buttonHome = findViewById(R.id.button_home);
+        buttonChat = findViewById(R.id.button_chat);
+        buttonMy = findViewById(R.id.button_my);
+        buttonWarehouse = findViewById(R.id.button_warehouse);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.chat1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/fyt/StoreActivity.java
+++ app/src/main/java/com/example/fyt/StoreActivity.java
@@ -15,7 +15,7 @@ public class StoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_store);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.store), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
